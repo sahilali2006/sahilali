682a9ba909dcc8ba660e41729914cf884197e63c
Sahil Ali <sahilali233310@gmail.com>
2026-04-02 10:08:32 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -4,7 +4,6 @@ Write a program that take user age as input ( ex: 25 ) and show if they can vote
 #include<stdio.h>
 int main(){
 int age;
-
 printf("Enter age = ");
 scanf("%d",&age);
 age>18 ? printf("you can vote ") : printf("you can't vote ");
